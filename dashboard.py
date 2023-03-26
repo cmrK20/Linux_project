d9f269f5b281ff362bf5df5704ef8ad9331dc443
EC2 Default User <ec2-user@ip-172-31-5-119.eu-north-1.compute.internal>
2023-03-26 22:23:03 +0000

    Initial commit

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
+from datetime import datetime, timedelta
 
 app = dash.Dash(__name__)
 
@@ -21,21 +22,31 @@ app.layout = html.Div([
 def update_dashboard(_):
     data = pd.read_csv('eth_price_history.csv', names=['timestamp', 'price'])
     data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data['price'] = data['price'].astype(float)
+    data = data.sort_values(by='timestamp')
+    
     fig = px.line(data, x='timestamp', y='price', title='Ethereum Price Time Series')
+    fig.update_layout(xaxis_range=[pd.Timestamp.today().strftime('%Y-%m-%d'), pd.Timestamp.today().strftime('%Y-%m-%d 23:59:59')])
 
     # Daily report calculation
-    daily_data = data[data['timestamp'].dt.hour == 20].copy()
+    now = datetime.now()
+    if now.hour >= 8:
+        daily_data = data[data['timestamp'].dt.date == now.date()].copy()
+    else:
+        yesterday = now - timedelta(days=1)
+        daily_data = data[data['timestamp'].dt.date == yesterday.date()].copy()
     daily_data['change'] = daily_data['price'].pct_change() * 100
     daily_data['volatility'] = daily_data['change'].rolling(window=2).std()
+    daily_data['volatility'] = daily_data['volatility'].fillna(0)
 
     # Daily report display
     report = html.Table([
-        html.Tr([html.Td('Date'), html.Td('Price'), html.Td('Change (%)'), html.Td('Volatility')])
+        html.Tr([html.Th('Date'), html.Th('Price'), html.Th('Change (%)'), html.Th('Volatility')])
     ] + [
-        html.Tr([html.Td(row['timestamp'].strftime('%Y-%m-%d')),
-                 html.Td(f"{row['price']:.2f}"),
-                 html.Td(f"{row['change']:.2f}"),
-                 html.Td(f"{row['volatility']:.2f}")]) for _, row in daily_data.iterrows()
+        html.Tr([html.Td(daily_data['timestamp'].iloc[-1].strftime('%Y-%m-%d')),
+                 html.Td(f"{daily_data['price'].iloc[-1]:.2f}"),
+                 html.Td(f"{daily_data['change'].iloc[-1]:.2f}"),
+                 html.Td(f"{daily_data['volatility'].iloc[-1]:.2f}")])
     ])
 
     return fig, report
--- scrape_eth_price.sh
+++ scrape_eth_price.sh
@@ -2,6 +2,11 @@
 
 url="https://www.coindesk.com/price/ethereum/"
 html="$(curl -s $url)"
-price=$(echo "$html" | grep -oP '(?<=<span class="currency-pricestyles__Price-sc-1rux8hj-0 jIzQOt">)[^<]*')
+price=$(echo "$html" | grep -oP '(?<=<span class="currency-pricestyles__Price-sc-1rux8hj-0 jIzQOt">)[^<]*' | tr -d ',')
 
-echo $(date +%Y-%m-%d,%H:%M:%S),$price >> eth_price_history.csv
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$timestamp,$price" >> /home/ec2-user/Linux_project/eth_price_history.csv
+
+# Add logging
+echo "[$timestamp] Price fetched: $price" >> /home/ec2-user/Linux_project/scrape_eth_price.log
+
